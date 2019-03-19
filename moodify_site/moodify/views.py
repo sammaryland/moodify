b8c866125dd123c7dbb503ea66697a6fdf17f1dc
swmaryland <swmaryland@crimson.ua.edu>
2019-03-19 17:30:14 -0500

    can find tweets based on username

---
+++ moodify_site/moodify/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django import forms
+
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+import tweepy
+
+class UserName(forms.Form):
+  name = forms.CharField(label='Username', max_length=40)
+
+def get_tweets(request):
+  if 'Username' in request.GET:
+    uname = request.GET['Username']
+    
+    consumer_key = "ckWshdUaNqkODubT0ud1aYTwZ"
+    consumer_secret = "aBahFPcKiAFuH6UnyY1RS1TCPHpUZTAiDqutfROjQC4h1L20Bp"
+    access_key = "354408891-XPtSKVLf38W2ECToclWdPpL3FwjUVJAmvecILDLU"
+    access_secret = "Guh9GvZCRC76jDwx8rTskVhlZHnB5JMXFdc6KPD4mYql8"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    new_tweets = api.user_timeline(screen_name=uname, count=10, tweet_mode="extended")
+    return render(request, 'tweet_results.html', {'uname': uname, 'tweets': new_tweets})
+  
+  else:
+    message = 'Empty Form'
+  
+  return HttpResponse(message)
+
+
+@ensure_csrf_cookie
+def index(request):
+    return render_to_response('index.html')
---
+++ moodify_site/moodify_site/urls.py
@@ -0,0 +1,26 @@
+"""moodify_site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from moodify import views as moodify_views
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', moodify_views.index, name="home"),
+    url(r'^get_tweets/$', csrf_exempt(moodify_views.get_tweets), name="get_tweets"),
+]
