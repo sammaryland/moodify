2e0505f46d858984d58b1f40aa766582bc609215
swmaryland <swmaryland@crimson.ua.edu>
2019-03-19 18:20:27 -0500

    hiding api key

--- moodify_site/moodify/config.py
+++ moodify_site/moodify/config.py
@@ -1,4 +1,4 @@
-consumer_key = "ckWshdUaNqkODubT0ud1aYTwZ"
-consumer_secret = "aBahFPcKiAFuH6UnyY1RS1TCPHpUZTAiDqutfROjQC4h1L20Bp"
-access_key = "354408891-XPtSKVLf38W2ECToclWdPpL3FwjUVJAmvecILDLU"
-access_secret = "Guh9GvZCRC76jDwx8rTskVhlZHnB5JMXFdc6KPD4mYql8"
+consumer_key = "YOUR_KEY"
+consumer_secret = "YOUR_SECRET"
+access_key = "YOUR_ACCESS_KEY"
+access_secret = "YOUR_ACCESS_SECRET"
