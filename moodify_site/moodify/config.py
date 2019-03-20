0b6a4f29aaf80836d1766f8bf77cc4ec2ef7d0fc
swmaryland <swmaryland@crimson.ua.edu>
2019-03-19 22:48:49 -0500

    tone analysis

--- moodify_site/moodify/config.py
+++ moodify_site/moodify/config.py
@@ -2,3 +2,5 @@ consumer_key = "ckWshdUaNqkODubT0ud1aYTwZ"
 consumer_secret = "aBahFPcKiAFuH6UnyY1RS1TCPHpUZTAiDqutfROjQC4h1L20Bp"
 access_key = "354408891-XPtSKVLf38W2ECToclWdPpL3FwjUVJAmvecILDLU"
 access_secret = "Guh9GvZCRC76jDwx8rTskVhlZHnB5JMXFdc6KPD4mYql8"
+
+watson_api_key = "EStPh4O30AR8mD2256cdS8W6e_FJ4gShgUYCIg3XyN0l"
--- moodify_site/moodify/views.py
+++ moodify_site/moodify/views.py
@@ -13,9 +13,35 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 import tweepy
+from watson_developer_cloud import ToneAnalyzerV3
+import urllib, json
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.path.append(".")
+
+tone_analyzer = ToneAnalyzerV3(
+  iam_apikey=config.watson_api_key,
+  version='2017-09-21'
+)
 
 class UserName(forms.Form):
   name = forms.CharField(label='Username', max_length=40)
+
+def analyze_tweets(tweets):
+  results = {}
+  for tweet in tweets:
+    json_output = tone_analyzer.tone(tweet.full_text, content_type='text/plain').get_result()
+    for i in json_output['document_tone']['tones']:
+      n = i['tone_name']
+      s = i['score']
+      if n not in results:
+        results[n] = s
+      results[n] = (results[n] + s) / 2
+  for k, v in results.items():
+    print str(k) + "\t" + str(v)
+  return results
 
 def get_tweets(request):
   if 'Username' in request.GET:
@@ -25,8 +51,9 @@ def get_tweets(request):
     auth.set_access_token(config.access_key, config.access_secret)
     api = tweepy.API(auth)
 
-    new_tweets = api.user_timeline(screen_name=uname, tweet_mode="extended")
-    return render(request, 'tweet_results.html', {'uname': uname, 'tweets': new_tweets})
+    new_tweets = api.user_timeline(screen_name=uname, count=10, tweet_mode="extended")
+    results = analyze_tweets(new_tweets)
+    return render(request, 'tweet_results.html', {'uname': uname, 'tweets': new_tweets, 'results': results})
   
   else:
     message = 'Empty Form'
