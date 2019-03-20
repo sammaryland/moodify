68d6bb5fb7061acaf07793b28ec9b323ee251abd
swmaryland <swmaryland@crimson.ua.edu>
2019-03-20 11:50:06 -0500

    API keys

--- moodify_site/moodify/config.py
+++ moodify_site/moodify/config.py
@@ -1,6 +1,8 @@
+# Twitter API Keys
 consumer_key = "CONSUMER_KEY"
 consumer_secret = "CONSUMER_SECRET"
 access_key = "ACCESS_KEY"
 access_secret = "ACCESS_SECRET"
 
+# Watson Tone Analyzer API Keys
 watson_api_key = "WATSON_API_KEY"
